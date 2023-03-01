3f352b2490ea4fef864efaac0e284871c2b38438
unknown <alyosha.plotnikov2001@mail.ru>
2023-03-01 21:12:00 +0400

    `profile`

--- job_search/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-import datetime
-
-
-def home(request):
-    date = datetime.datetime.now().date()
-    name = 'Dave'
-    context_ = {'date': date, 'name': name}
-    return render(request, 'base.html', context_)
